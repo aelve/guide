eb89d3230d234b212d9d5ba312edec14ea3bd15f
Artyom <yom@artyom.me>
2016-02-14 15:19:36 +0300

    The ability to add pros and cons

--- src/Main.hs
+++ src/Main.hs
@@ -12,10 +12,10 @@ module Main (main) where
 
 -- General
 import BasePrelude hiding (Category)
+-- Monads and monad transformers
+import Control.Monad.State
 -- Lenses
 import Lens.Micro.Platform
--- IO
-import Control.Monad.IO.Class
 -- Text
 import Data.Text (Text)
 import qualified Data.Text as T
@@ -25,42 +25,51 @@ import qualified Data.Text.Format as Format
 import Data.Text.Format.Params (Params)
 -- Web
 import Lucid hiding (for_)
-import Web.Spock
+import Web.Spock hiding (get)
+import qualified Web.Spock as Spock
 import Network.Wai.Middleware.Static
 
+
+-- | Unique id, used for categories and items.
+type UID = Int
 
 data ItemKind = HackageLibrary | Library | Unknown
 
 data Item = Item {
-  _name :: Text,
-  _pros :: [Text],
-  _cons :: [Text],
-  _link :: Maybe Text,
-  _kind :: ItemKind }
+  _itemId :: UID,
+  _name   :: Text,
+  _pros   :: [Text],
+  _cons   :: [Text],
+  _link   :: Maybe Text,
+  _kind   :: ItemKind }
 
 makeLenses ''Item
 
 data Category = Category {
-  _catId :: Int,
+  _catId :: UID,
   _title :: Text,
   _items :: [Item] }
 
 makeLenses ''Category
 
 data S = S {
-  _nextId :: Int,
+  _nextId :: UID,
   _categories :: [Category] }
 
 makeLenses ''S
 
-categoryById :: Int -> Traversal' S Category
-categoryById i = categories . each . filtered ((== i) . view catId)
+categoryById :: UID -> Traversal' S Category
+categoryById uid = categories . each . filtered ((== uid) . view catId)
 
-newId :: IORef S -> IO Int
+itemById :: UID -> Traversal' S Item
+itemById uid = categories . each .
+               items . each . filtered ((== uid) . view itemId)
+
+newId :: IORef S -> IO UID
 newId s = do
-  i <- view nextId <$> readIORef s
+  uid <- view nextId <$> readIORef s
   modifyIORef s (nextId %~ succ)
-  return i
+  return uid
 
 emptyState :: S
 emptyState = S {
@@ -69,24 +78,26 @@ emptyState = S {
 
 sampleState :: S
 sampleState = S {
-  _nextId = 1,
+  _nextId = 3,
   _categories = [
     Category {
       _catId = 0,
       _title = "lenses",
       _items = [
         Item {
-          _name = "lens",
-          _pros = ["the standard lenses library", "batteries included"],
-          _cons = ["huge"],
-          _link = Nothing,
-          _kind = HackageLibrary },
+          _itemId = 1,
+          _name   = "lens",
+          _pros   = ["the standard lenses library", "batteries included"],
+          _cons   = ["huge"],
+          _link   = Nothing,
+          _kind   = HackageLibrary },
         Item {
-          _name = "microlens",
-          _pros = ["very small", "good for libraries"],
-          _cons = ["doesn't have advanced features"],
-          _link = Nothing,
-          _kind = HackageLibrary }
+          _itemId = 2,
+          _name   = "microlens",
+          _pros   = ["very small", "good for libraries"],
+          _cons   = ["doesn't have advanced features"],
+          _link   = Nothing,
+          _kind   = HackageLibrary }
       ] }
   ] }
 
@@ -94,35 +105,61 @@ main :: IO ()
 main = runSpock 8080 $ spockT id $ do
   middleware (staticPolicy (addBase "static"))
   stateVar <- liftIO $ newIORef sampleState
+  let withS f = liftIO $ atomicModifyIORef' stateVar (swap . runState f)
 
-  get root $ do
+  Spock.get root $ do
     s <- liftIO $ readIORef stateVar
     lucid $ renderRoot s
 
-  post "/add/category" $ do
+  -- The “/add” methods return rendered parts of the structure (added
+  -- categories, changed items, etc) so that the Javascript part could take
+  -- them and inject into the page. We don't want to duplicate rendering on
+  -- server side and on client side.
+
+  Spock.post "/add/category" $ do
     title' <- param' "title"
     id' <- liftIO (newId stateVar)
     let newCategory = Category {
           _catId = id',
           _title = title',
           _items = [] }
-    liftIO $ modifyIORef stateVar $
-      categories %~ (++ [newCategory])
+    withS $
+      categories %= (++ [newCategory])
     lucid $ renderCategory newCategory
 
-  post ("/add/item/library" <//> var) $ \catId' -> do
+  Spock.post ("/add/item/library" <//> var) $ \catId' -> do
     name' <- param' "name"
+    id' <- liftIO (newId stateVar)
     let newItem = Item {
-          _name = name',
-          _pros = [],
-          _cons = [],
-          _link = Nothing,
-          _kind = HackageLibrary }
+          _itemId = id',
+          _name   = name',
+          _pros   = [],
+          _cons   = [],
+          _link   = Nothing,
+          _kind   = HackageLibrary }
     -- TODO: maybe do something if the category doesn't exist (e.g. has been
     -- already deleted)
-    liftIO $ modifyIORef stateVar $
-      categoryById catId' . items %~ (++ [newItem])
+    withS $
+      categoryById catId' . items %= (++ [newItem])
     lucid $ renderItem newItem
+
+  Spock.post ("/add/pros" <//> var) $ \itemId' -> do
+    content <- param' "content"    
+    changedItems <- withS $ do
+      itemById itemId' . pros %= (++ [content])
+      gets (^.. itemById itemId')
+    case changedItems of
+      [x] -> lucid $ renderItem x
+      _   -> return ()
+
+  Spock.post ("/add/cons" <//> var) $ \itemId' -> do
+    content <- param' "content"    
+    changedItems <- withS $ do
+      itemById itemId' . cons %= (++ [content])
+      gets (^.. itemById itemId')
+    case changedItems of
+      [x] -> lucid $ renderItem x
+      _   -> return ()
 
 renderRoot :: S -> Html ()
 renderRoot s = do
@@ -131,10 +168,8 @@ renderRoot s = do
   includeCSS "/css.css"
   div_ [id_ "categories"] $ do
     mapM_ renderCategory (s ^. categories)
-  let handler = "if (event.keyCode == 13) {\
-                \  addCategory(this.value);\
-                \  this.value = ''; }"
-  input_ [type_ "text", placeholder_ "new category", onkeyup_ handler]
+  let handler = "addCategory(this.value);"
+  input_ [type_ "text", placeholder_ "new category", submitFunc handler]
 
 renderCategory :: Category -> Html ()
 renderCategory category =
@@ -145,26 +180,26 @@ renderCategory category =
     -- whether it has to be updated.
     div_ [class_ "items"] $
       mapM_ renderItem (category^.items)
-    -- TODO: probably move handlers to the Javascript part
-    let handler = format "if (event.keyCode == 13) {\
-                         \  addLibrary({}, this.value);\
-                         \  this.value = ''; }"
-                         [category^.catId]
-    input_ [type_ "text", placeholder_ "new item", onkeyup_ handler]
+    let handler = format "addLibrary({}, this.value);" [category^.catId]
+    input_ [type_ "text", placeholder_ "new item", submitFunc handler]
 
 -- TODO: when the link for a HackageLibrary isn't empty, show it separately
 -- (as “site”), don't replace the Hackage link
 renderItem :: Item -> Html ()
 renderItem item =
-  div_ [class_ "item"] $ do
+  div_ [class_ "item", id_ (format "item{}" [item^.itemId])] $ do
     h3_ itemHeader
     div_ [class_ "pros-cons"] $ do
       div_ [class_ "pros"] $ do
         p_ "Pros:"
         ul_ $ mapM_ (li_ . toHtml) (item^.pros)
+        let handler = format "addPros({}, this.value);" [item^.itemId]
+        input_ [type_ "text", placeholder_ "add pros", submitFunc handler]
       div_ [class_ "cons"] $ do
         p_ "Cons:"
         ul_ $ mapM_ (li_ . toHtml) (item^.cons)
+        let handler = format "addCons({}, this.value);" [item^.itemId]
+        input_ [type_ "text", placeholder_ "add cons", submitFunc handler]
   where
     hackageLink = format "https://hackage.haskell.org/package/{}"
                          [item^.name]
@@ -182,6 +217,13 @@ includeJS url = with (script_ "") [src_ url]
 
 includeCSS :: Text -> Html ()
 includeCSS url = link_ [rel_ "stylesheet", type_ "text/css", href_ url]
+
+submitFunc :: Text -> Attribute
+submitFunc f = onkeyup_ $ format
+  "if (event.keyCode == 13) {\
+  \  {}\
+  \  this.value = ''; }"
+  [f]
 
 lucid :: Html a -> ActionT IO a
 lucid = html . TL.toStrict . renderText
--- static/js.js
+++ static/js.js
@@ -13,3 +13,23 @@ function addCategory(s) {
      $("#categories").append(data);
      });
   }
+
+// add pros to some item
+function addPros(itemId, s) {
+  $.post("/add/pros/" + itemId, {content: s})
+   .done(function(data) {
+     setItemHtml(itemId, data);
+     });
+  }
+
+// add cons to some item
+function addCons(itemId, s) {
+  $.post("/add/cons/" + itemId, {content: s})
+   .done(function(data) {
+     setItemHtml(itemId, data);
+     });
+  }
+
+// reload an item
+function setItemHtml(itemId, data) {
+  $("#item"+itemId).replaceWith(data) }
