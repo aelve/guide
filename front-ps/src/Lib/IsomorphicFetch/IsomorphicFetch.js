f98a8dcb3f7bdf66616c2d0fbff849f34ab71cbc
Jens Krause <email@jkrause.io>
2017-07-04 19:37:31 +0200

    Fix FFI of `isomorphic-fetch` (#166)

--- front-ps/src/Lib/IsomorphicFetch/IsomorphicFetch.js
+++ front-ps/src/Lib/IsomorphicFetch/IsomorphicFetch.js
@@ -1,17 +1,18 @@
 
-require('es6-promise').polyfill();
 require('isomorphic-fetch');
 
-exports.fetchImpl = function(uri, done, fail) {
-  return fetch(uri)
-  	.then( function(response) {
-      const status = response.status;
-  		if (status >= 400) {
-        return fail('Bad response from server -> status : ' + status);
-  		}
-  		return response.json();
-  	})
-    .then(function(data) {
-  		done(data)();
-  	});
+exports.fetchImpl = function(uri, successCb, errCb) {
+  return function () {
+    return fetch(uri)
+    	.then( function(response) {
+        const status = response.status;
+    		if (status >= 400) {
+          return errCb('Bad response from server -> status : ' + status)();
+    		}
+    		return response.json();
+    	})
+      .then(function(data) {
+        successCb(data)();
+      });
+    };
   };
