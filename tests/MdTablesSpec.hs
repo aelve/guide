57d3a735d10e304da8bf8adb5565fa6b5225b59c
vrom911 <vrom911@gmail.com>
2017-08-01 16:52:04 +0300

    [#177] Prettify tests for persing

--- tests/MdTablesSpec.hs
+++ tests/MdTablesSpec.hs
@@ -1,107 +1,166 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-
 module MdTablesSpec (tests) where
 
-import BasePrelude
+import           BasePrelude
 -- CMark
-import qualified CMark as MD
+import qualified CMark          as MD
 -- Text
-import qualified Data.Text.All as T
-import Data.Text.All (Text)
+import           Data.Text.All  (Text)
+import qualified Data.Text.All  as T
 -- Testing
-import Test.Hspec
+import           Test.Hspec
 
-import Guide.Markdown
+import           Guide.Markdown
 
 tests :: Spec
 tests =
   describe "MdTables.table" $
     describe "getting" $ do
+      it "return simple table with column names from list" $
+        getTable simpleTableColListMD `shouldBe` Just simpleTable
+      it "return simple table with column names from text with separator" $
+        getTable simpleTableMD `shouldBe` Just simpleTable
       it "returns valid full example table" $
-        getTable fullTableMD `shouldBe` (Just $ fullTable (Just columnNames) 12)
+        getTable fullTableMD `shouldBe` Just fullTable
       it "returns nothing for table without hr" $
-        getTable tableWOBreak `shouldBe` Nothing
+        getTable tableWOBreakMD `shouldBe` Nothing
       it "returns table without column names" $
-        getTable tableWOColumnNames `shouldBe` (Just $ fullTable Nothing 9)
+        getTable tableWOColumnNamesMD `shouldBe` Just tableWOColumnNames
 
+-----------------------
+------ Text Md --------
+-----------------------
 
-textTable :: [Text]
-textTable =
-  [ "+ %TABLE Table"
-  , "+ - Column 1"
+tableKeyword :: Text
+tableKeyword = "+ %TABLE Table"
+
+columnNamesList :: [Text]
+columnNamesList =
+  [ "+ - Column 1"
   , "  - Column 2"
   , "  - Column 3"
-  , "+ ----------------"
-  , "+ - *foo* | **bar** | baz"
-  , ""
-  , "+ - Foo"
+  ]
+
+columnNamesSimple :: Text
+columnNamesSimple = "+ - Column 1|Column 2|Column 3"
+
+breakText :: Text
+breakText = "+ ----------------"
+
+rowOfList :: [Text]
+rowOfList =
+  [ "+ - Foo"
   , "  - Bar"
   , "  - Baz"
   , ""
-  , "+ - ```"
+  ]
+
+rowWithSeparator :: Text
+rowWithSeparator = "+ - Another foo | Another bar | Another baz"
+
+rowSeveralNodes :: Text
+rowSeveralNodes = "+ - *foo* | **bar** | baz"
+
+rowOfListSeveralNodes :: [Text]
+rowOfListSeveralNodes =
+  [ "+ - ```"
   , "    Code foo"
   , "    ```"
   , "  - Simple bar"
   , "  - `inline code` baz"
-  , "+ - Another foo | Another bar | Another baz"
   ]
 
-buildTable :: [Text] -> MD.Node
-buildTable txtTable = head $ parseMD $ T.unlines txtTable
-
-fullTableMD :: MD.Node
-fullTableMD = buildTable textTable
-
-tableWOBreak :: MD.Node
-tableWOBreak = buildTable $ take 4 textTable ++ drop 5 textTable
-
-tableWOColumnNames :: MD.Node
-tableWOColumnNames = buildTable $ take 1 textTable ++ drop 4 textTable
+---------------------------
+------- MD elements -------
+---------------------------
 
 columnNames :: [[MD.Node]]
 columnNames = [ [ MD.Node Nothing (MD.TEXT "Column 1") [] ]
               , [ MD.Node Nothing (MD.TEXT "Column 2") [] ]
               , [ MD.Node Nothing (MD.TEXT "Column 3") [] ]
               ]
-fullTable :: Maybe [[MD.Node]] -> Int -> MarkdownTable
-fullTable colNm n = MarkdownTable { markdownTableName = "Table"
-                      , markdownTableColumns = colNm
-                      , markdownTableRows =
-                          [ [ [ MD.Node Nothing MD.EMPH [ MD.Node Nothing (MD.TEXT "foo") [] ]
-                              , MD.Node Nothing (MD.TEXT " ") []
-                              ]
-                            , [ MD.Node Nothing (MD.TEXT " ") []
-                              , MD.Node Nothing MD.STRONG [ MD.Node Nothing (MD.TEXT "bar") [] ]
-                              , MD.Node Nothing (MD.TEXT " ") []
-                              ]
-                            , [ MD.Node Nothing (MD.TEXT " baz") [] ]
-                            ]
-                          , [ [ MD.Node Nothing (MD.TEXT "Foo") [] ]
-                            , [ MD.Node Nothing (MD.TEXT "Bar") [] ]
-                            , [ MD.Node Nothing (MD.TEXT "Baz") [] ]
-                            ]
-                          , [ [ MD.Node ( Just
-                                          MD.PosInfo
-                                        { startLine = n
-                                        , startColumn = 5
-                                        , endLine = n + 2
-                                        , endColumn = 7
-                                        })
-                                  (MD.HTML_BLOCK
-                                     "<div class=\"sourceCode\"><pre class=\"sourceCode\"><code class=\"sourceCode\">Code foo</code></pre></div>")
-                                  []
-                              ]
-                            , [ MD.Node Nothing (MD.TEXT "Simple bar") [] ]
-                            , [ MD.Node Nothing (MD.CODE "inline code") []
-                              ,  MD.Node Nothing (MD.TEXT " baz") []
-                              ]
-                            ]
-                          , [ [ MD.Node Nothing (MD.TEXT "Another foo ") [] ]
-                            , [ MD.Node Nothing (MD.TEXT " Another bar ") [] ]
-                            , [ MD.Node Nothing (MD.TEXT " Another baz") [] ]
-                            ]
-                          ]
-                      }
 
+rowOfListMD :: [[MD.Node]]
+rowOfListMD = [ [ MD.Node Nothing (MD.TEXT "Foo") [] ]
+              , [ MD.Node Nothing (MD.TEXT "Bar") [] ]
+              , [ MD.Node Nothing (MD.TEXT "Baz") [] ]
+              ]
+
+rowSeveralNodesMD :: [[MD.Node]]
+rowSeveralNodesMD = [ [ MD.Node Nothing MD.EMPH [ MD.Node Nothing (MD.TEXT "foo") [] ]
+                      , MD.Node Nothing (MD.TEXT " ") []
+                      ]
+                    , [ MD.Node Nothing (MD.TEXT " ") []
+                      , MD.Node Nothing MD.STRONG [ MD.Node Nothing (MD.TEXT "bar") [] ]
+                      , MD.Node Nothing (MD.TEXT " ") []
+                      ]
+                    , [ MD.Node Nothing (MD.TEXT " baz") [] ]
+                    ]
+
+rowWithSeparatorMD :: [[MD.Node]]
+rowWithSeparatorMD = [ [ MD.Node Nothing (MD.TEXT "Another foo ") [] ]
+                     , [ MD.Node Nothing (MD.TEXT " Another bar ") [] ]
+                     , [ MD.Node Nothing (MD.TEXT " Another baz") [] ]
+                     ]
+
+rowOfListSeveralNodesMD :: Int -> [[MD.Node]]
+rowOfListSeveralNodesMD n =
+  [ [ MD.Node ( Just
+                MD.PosInfo
+              { startLine = n
+              , startColumn = 5
+              , endLine = n + 2
+              , endColumn = 7
+              })
+        (MD.HTML_BLOCK
+           "<div class=\"sourceCode\"><pre class=\"sourceCode\"><code class=\"sourceCode\">Code foo</code></pre></div>")
+        []
+    ]
+  , [ MD.Node Nothing (MD.TEXT "Simple bar") [] ]
+  , [ MD.Node Nothing (MD.CODE "inline code") []
+    ,  MD.Node Nothing (MD.TEXT " baz") []
+    ]
+  ]
+
+--------------------------------------
+
+buildTableMD :: [Text] -> MD.Node
+buildTableMD txtTable = head $ parseMD $ T.unlines txtTable
+
+fullTableMD :: MD.Node
+fullTableMD = buildTableMD
+  (tableKeyword:columnNamesList ++ breakText:rowSeveralNodes:rowOfList ++ rowOfListSeveralNodes ++ [rowWithSeparator])
+
+simpleTableMD :: MD.Node
+simpleTableMD = buildTableMD (tableKeyword:columnNamesSimple:breakText:rowOfList)
+
+simpleTableColListMD :: MD.Node
+simpleTableColListMD = buildTableMD (tableKeyword:columnNamesList ++ breakText:rowOfList)
+
+tableWOBreakMD :: MD.Node
+tableWOBreakMD = buildTableMD (tableKeyword:columnNamesSimple:rowOfList)
+
+tableWOColumnNamesMD :: MD.Node
+tableWOColumnNamesMD = buildTableMD (tableKeyword:breakText:rowOfList)
+
+buildTable :: Maybe [[MD.Node]] -> [[[MD.Node]]] -> MarkdownTable
+buildTable colNm rows =
+  MarkdownTable { markdownTableName = "Table"
+                , markdownTableColumns = colNm
+                , markdownTableRows = rows
+                }
+
+fullTable :: MarkdownTable
+fullTable = buildTable (Just columnNames) [ rowSeveralNodesMD
+                                     , rowOfListMD
+                                     , rowOfListSeveralNodesMD 12
+                                     , rowWithSeparatorMD
+                                     ]
+
+simpleTable :: MarkdownTable
+simpleTable = buildTable (Just columnNames) [rowOfListMD]
+
+tableWOColumnNames :: MarkdownTable
+tableWOColumnNames = buildTable Nothing [rowOfListMD]
