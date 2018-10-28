d510a06cf2e275208f0b1cce67ebf91803103de3
zeot <kotoraxia@gmail.com>
2018-10-28 17:47:23 +0300

    Logo added

---
+++ front/client/components/Logo.vue
@@ -0,0 +1,23 @@
+<template>
+  <router-link
+    class="logo"
+    :to="{ name: 'Index' }"
+  >
+    Aelve Guide
+  </router-link>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component
+export default class Logo extends Vue {}
+</script>
+
+
+<style scoped>
+.logo {
+  text-decoration: none;
+  color: #ffffff;
+}
+</style>
