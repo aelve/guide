eafe87bd0af2ce9b38dd321be5c70b0a6d6ce3bd
Vladislav Sabanov <vladislav.sabanov@gmail.com>
2018-11-04 00:09:19 +0500

    Add setCategoryNote and setCategoryInfo and setItemInfo (#227)
    * Add setCategoryNote
    
    * Fix cache function
    
    * Fix style
    
    * Add setCategoryInfo
    
    * Fix instances, add throwErrorand unH
    
    * WIP
    
    * Add setItemHackage on old kind + commented new realisation
    
    * Refactor and fix bug
    
    * Update src/Guide/Api/Methods.hs
    
    Co-Authored-By: willbasky <vladislav.sabanov@gmail.com>
    
    * Add a comment
    
    * Update src/Guide/Api/Methods.hs
    
    Co-Authored-By: willbasky <vladislav.sabanov@gmail.com>
    
    * Update src/Guide/Api/Methods.hs
    
    Co-Authored-By: willbasky <vladislav.sabanov@gmail.com>
    
    * Update src/Guide/Api/Methods.hs
    
    Co-Authored-By: willbasky <vladislav.sabanov@gmail.com>
    
    * Update src/Guide/Api/Methods.hs
    
    Co-Authored-By: willbasky <vladislav.sabanov@gmail.com>
    
    * Update src/Guide/Api/Methods.hs
    
    Co-Authored-By: willbasky <vladislav.sabanov@gmail.com>
    
    * Update src/Guide/Api/Methods.hs
    
    Co-Authored-By: willbasky <vladislav.sabanov@gmail.com>
    
    * Update src/Guide/Api/Types.hs
    
    Co-Authored-By: willbasky <vladislav.sabanov@gmail.com>
    
    * Update src/Guide/Api/Utils.hs
    
    Co-Authored-By: willbasky <vladislav.sabanov@gmail.com>
    
    * Fix FromJSON ItemKind instance
    
    * Update src/Guide/Api/Methods.hs
    
    Co-Authored-By: willbasky <vladislav.sabanov@gmail.com>

--- src/Guide/Api/Methods.hs
+++ src/Guide/Api/Methods.hs
@@ -13,11 +13,13 @@ import Data.Text (Text)
 import Servant
 
 import Guide.Api.Types
+import Guide.Api.Utils
 import Guide.Cache
 import Guide.State
 import Guide.Types
 import Guide.Utils
 
+import qualified Data.Set as S
 import qualified Data.Text as T
 import qualified Guide.Search as Search
 
@@ -44,10 +46,12 @@ getCategory db catId =
 -- category with this title exists already).
 createCategory :: DB -> Text -> Text -> Handler (Uid Category)
 createCategory db title' group' = do
+  when (T.null title') $ do throwError (err400 {errBody = "Title not provided"})
+  when (T.null group') $ do throwError (err400 {errBody = "Group' not provided"})
   -- If the category exists already, don't create it
   cats <- view categories <$> dbQuery db GetGlobalState
   let isDuplicate cat = T.toCaseFold (cat^.title) == T.toCaseFold title'
-                     && T.toCaseFold (cat^.group_) == T.toCaseFold group'
+        && T.toCaseFold (cat^.group_) == T.toCaseFold group'
   case find isDuplicate cats of
     Just c  -> return (c^.uid)
     Nothing -> do
@@ -58,12 +62,43 @@ createCategory db title' group' = do
       -- TODO addEdit edit
       return catId
 
+-- | Edit categoty's note.
+setCategoryNotes :: DB -> Uid Category -> Text -> Handler NoContent
+setCategoryNotes db catId note = uncache db (CacheCategoryNotes catId) $ do
+  dbQuery db (GetCategoryMaybe catId) >>= \case
+    Nothing -> throwError (err404 {errBody = "Category not found"})
+    Just _ -> do
+      (_edit, _newCategory) <- dbUpdate db (SetCategoryNotes catId note)
+      -- TODO diff and merge
+      pure NoContent
+
+-- | Edit category's info (title, group, status, sections (pro/con, ecosystem, note)).
+setCategoryInfo :: DB -> Uid Category -> CCategoryInfoEdit -> Handler NoContent
+setCategoryInfo db catId CCategoryInfoEdit{..} = uncache db (CacheCategoryInfo catId) $ do
+  dbQuery db (GetCategoryMaybe catId) >>= \case
+    Nothing -> throwError (err404 {errBody = "Category not found"})
+    Just category -> do
+      -- TODO diff and merge
+      _ <- dbUpdate db $ SetCategoryTitle catId $ unH ccieTitle
+      _ <- dbUpdate db $ SetCategoryGroup catId $ unH ccieGroup
+      _ <- dbUpdate db $ SetCategoryStatus catId $ unH ccieStatus
+      let oldEnabledSections = category ^. enabledSections
+      let newEnabledSections = unH ccieSections
+      _ <- dbUpdate db $ ChangeCategoryEnabledSections catId
+          (newEnabledSections S.\\ oldEnabledSections)
+          (oldEnabledSections S.\\ newEnabledSections)
+      -- TODO record edits
+      pure NoContent
+
 -- | Delete a category.
 deleteCategory :: DB -> Uid Category -> Handler NoContent
 deleteCategory db catId = uncache db (CacheCategory catId) $ do
-  _mbEdit <- dbUpdate db (DeleteCategory catId)
-  pure NoContent
-  -- TODO mapM_ addEdit mbEdit
+  dbQuery db (GetCategoryMaybe catId) >>= \case
+    Nothing -> throwError (err404 {errBody = "Category not found"})
+    Just _ -> do
+      _mbEdit <- dbUpdate db (DeleteCategory catId)
+      pure NoContent
+      -- TODO mapM_ addEdit mbEdit
 
 ----------------------------------------------------------------------------
 -- Items
@@ -75,21 +110,37 @@ deleteCategory db catId = uncache db (CacheCategory catId) $ do
 -- with duplicated names.
 createItem :: DB -> Uid Category -> Text -> Handler (Uid Item)
 createItem db catId name' = do
-  -- TODO: do something if the category doesn't exist (e.g. has been
-  -- already deleted)
-  itemId <- randomShortUid
-  -- If the item name looks like a Hackage library, assume it's a Hackage
-  -- library.
-  let isAllowedChar c = isAscii c && (isAlphaNum c || c == '-')
-      looksLikeLibrary = T.all isAllowedChar name'
-      kind' = if looksLikeLibrary then Library (Just name') else Other
-  time <- liftIO getCurrentTime
-  (_edit, _newItem) <- dbUpdate db (AddItem catId itemId name' time kind')
-  invalidateCache' db (CacheItem itemId)
-  -- TODO: addEdit edit
-  pure itemId
+  dbQuery db (GetCategoryMaybe catId) >>= \case
+    Nothing -> throwError (err404 {errBody = "Category not found"})
+    Just _ -> do
+      if T.null name' then throwError (err400 {errBody = "Name not provided"})
+      else do
+        itemId <- randomShortUid
+        -- If the item name looks like a Hackage library, assume it's a Hackage
+        -- library.
+        let isAllowedChar c = isAscii c && (isAlphaNum c || c == '-')
+            looksLikeLibrary = T.all isAllowedChar name'
+            kind' = if looksLikeLibrary then Library (Just name') else Other
+        time <- liftIO getCurrentTime
+        (_edit, _newItem) <- dbUpdate db (AddItem catId itemId name' time kind')
+        invalidateCache' db (CacheItem itemId)
+        -- TODO: addEdit edit
+        pure itemId
 
 -- TODO: move an item
+
+-- | Set item's info
+setItemInfo :: DB -> Uid Item -> CItemInfo -> Handler NoContent
+setItemInfo db itemId CItemInfo{..} = uncache db (CacheItemInfo itemId) $ do
+  dbQuery db (GetItemMaybe itemId) >>= \case
+    Nothing -> throwError (err404 {errBody = "Item not found"})
+    Just _ -> do
+      -- TODO diff and merge
+      _ <- dbUpdate db $ SetItemName itemId $ unH ciiName
+      _ <- dbUpdate db $ SetItemGroup itemId $ unH ciiGroup
+      _ <- dbUpdate db $ SetItemLink itemId $ unH ciiLink
+      _ <- dbUpdate db $ SetItemKind itemId $ unH ciiKind
+      pure NoContent
 
 -- | Delete an item.
 deleteItem :: DB -> Uid Item -> Handler NoContent
@@ -107,19 +158,21 @@ deleteItem db itemId = uncache db (CacheItem itemId) $ do
 -- | Create a trait (pro/con).
 createTrait :: DB -> Uid Item -> TraitType -> Text -> Handler (Uid Trait)
 createTrait db itemId traitType text = do
-    traitId <- randomShortUid
-    (_edit, _newTrait) <- case traitType of
-        Con -> dbUpdate db (AddCon itemId traitId text)
-        Pro -> dbUpdate db (AddPro itemId traitId text)
-    invalidateCache' db (CacheItemTraits itemId)
--- TODO: mapM_ addEdit mbEdit
-    pure traitId
+  when (T.null text) $ throwError (err400 {errBody = "Trait text not provided"})
+  traitId <- randomShortUid
+  (_edit, _newTrait) <- case traitType of
+    Con -> dbUpdate db (AddCon itemId traitId text)
+    Pro -> dbUpdate db (AddPro itemId traitId text)
+  invalidateCache' db (CacheItemTraits itemId)
+  -- TODO: mapM_ addEdit mbEdit
+  pure traitId
 
 -- | Update the text of a trait (pro/con).
 setTrait :: DB -> Uid Item -> Uid Trait -> Text -> Handler NoContent
 setTrait db itemId traitId text = uncache db (CacheItemTraits itemId) $ do
-    (_edit, _newTrait) <- dbUpdate db (SetTraitContent itemId traitId text)
-    pure NoContent
+  (_edit, _newTrait) <- dbUpdate db (SetTraitContent itemId traitId text)
+  -- TODO diff and merge
+  pure NoContent
 
 -- | Delete a trait (pro/con).
 deleteTrait :: DB -> Uid Item -> Uid Trait -> Handler NoContent
--- src/Guide/Api/Server.hs
+++ src/Guide/Api/Server.hs
@@ -35,14 +35,17 @@ import Data.Acid as Acid
 apiServer :: DB -> Site AsServer
 apiServer db = Site
   { _categorySite = toServant (CategorySite
-      { _getCategories  = getCategories db
-      , _getCategory    = getCategory db
-      , _createCategory = createCategory db
-      , _deleteCategory = deleteCategory db }
+      { _getCategories    = getCategories db
+      , _getCategory      = getCategory db
+      , _createCategory   = createCategory db
+      , _setCategoryNotes = setCategoryNotes db
+      , _setCategoryInfo  = setCategoryInfo db
+      , _deleteCategory   = deleteCategory db }
       :: CategorySite AsServer)
 
   , _itemSite = toServant (ItemSite
       { _createItem     = createItem db
+      , _setItemInfo    = setItemInfo db
       , _deleteItem     = deleteItem db }
       :: ItemSite AsServer)
 
