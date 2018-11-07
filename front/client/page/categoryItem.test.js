fb377745d890d15b20ae5b789d92a8a50de4112c
Giyos <giyos.a@gmail.com>
2018-11-08 01:12:38 +0500

    add/delete item test v 0.0.1

--- front/client/page/categoryItem.test.js
+++ front/client/page/categoryItem.test.js
@@ -3,3 +3,20 @@ import VueSelector from 'testcafe-vue-selectors'
 
 fixture `ItemAddDelete`
   .page `http://localhost:5000/haskell`;
+
+test('Add New Item to category', async addItem => {
+  const addItemBtn
+  const addItemInput = Selector('input[aria-label="Item name"]')
+  const itemSubmitBtn
+  
+  await addItem
+  // необходимо кликнуть первую кнопку добавления item -> ввести название item -> кликнуть на submit
+  // Проверить появился ли item с таким name
+})
+
+test('Delete Item from category', async deleteItem => {
+  const delItemBtn
+  const delSubmitBtn
+  
+  await deleteItem
+})
