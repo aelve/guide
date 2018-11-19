4fb9d0e676af730b6dc8fca9a8c1f8fa68286698
Giyos <giyos.a@gmail.com>
2018-11-20 01:33:09 +0500

    minor add/delete item test refactoring

--- front/client/page/categoryItem.test.js
+++ front/client/page/categoryItem.test.js
@@ -12,23 +12,16 @@ test('Add New Item to category', async addItem => {
   await addItem
     .click(addItemBtn)
     .typeText(addItemInput, 'testcafe-item')
-    // .pressKey('enter') - when you click on enter some strange page reload occur
-    .click(itemSubmitBtn)
+    .pressKey('enter')
   for (let i = 0; Selector('.article-hd-textlg').length; i++) {
     await addItem.expect(Selector('.article-hd-textlg').nth(i).innerText).contains('testcafe-item')
   }
 })
 
 test('Delete Item from category', async deleteItem => {
-  // тут надо взять последнюю кнопку удаления, т.к. созданный выше item будет последним. 
-  // Нажать диалог => нажать submit => проверить что такого нет
-
   const delItemBtn = Selector(() => {
-    let buttons = document.querySelectorAll('.item-del-btn')
-
+    const buttons = document.querySelectorAll('.item-del-btn')
     let last = buttons[buttons.length - 1]
-
-    // return buttons.pop()
     return last
   })
 
