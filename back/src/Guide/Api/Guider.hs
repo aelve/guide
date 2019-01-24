ed2451544ee7d6274e30f3029115cd40157b885f
Dmitrij Pavluk <al.lastor@bk.ru>
2019-01-24 14:14:15 +0300

    monad DiT

--- back/src/Guide/Api/Guider.hs
+++ back/src/Guide/Api/Guider.hs
@@ -13,14 +13,21 @@ import Imports
 import Servant (Handler (..), ServantErr)
 import Servant.Server.Generic
 
+import qualified Control.Monad.Catch as Exc
+
 import Guide.Api.Utils (RequestDetails)
 import Guide.Config (Config)
 import Guide.State (DB)
+import Di.Core as Di (Di)
+import Di.Monad as Di
+import Df1
 
+type DefDiT = DiT Level Text Text IO
+type DefDi  = Di Level Text Text
 
 -- | A type for Guide handlers. Provides access to everything in 'Context'.
 newtype Guider a = Guider
-  { runGuider :: ReaderT Context IO a
+  { runGuider :: ReaderT Context DefDiT a
   } deriving (Functor, Applicative, Monad, MonadIO, MonadReader Context)
 
 -- | Context of Guider
@@ -36,11 +43,12 @@ instance MonadError ServantErr Guider where
 
   catchError :: Guider a -> (ServantErr -> Guider a) -> Guider a
   catchError (Guider m) f = Guider $ ReaderT $ \context ->
-    runReaderT m context `catch` (\err -> runReaderT (runGuider (f err)) context)
+    runReaderT m context `Exc.catch` (\err -> runReaderT (runGuider (f err)) context)
 
 -- | The custom type won't be accepted by servant server without this conventor used with 'hoistServer'.
-guiderToHandler :: Context -> Guider a -> Handler a
-guiderToHandler context (Guider m) = Handler $ ExceptT $ try $ runReaderT m context
+guiderToHandler :: Context -> DefDi -> Guider a -> Handler a
+guiderToHandler context di (Guider m) =
+  Handler $ ExceptT $ try $ runDiT di $ runReaderT m context
 
 -- | 'GuiderServer' used to create 'Guider' api.
 type GuiderServer = AsServerT Guider
