c038f19a148d5a50a10a1f399c340c107abf2dfd
Dmitrij Pavluk <al.lastor@bk.ru>
2019-01-24 15:14:19 +0300

    Logger.hs

---
+++ back/src/Guide/Api/Logger.hs
@@ -0,0 +1,9 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Guide.Api.Logger where
+
+import Imports
+
+import qualified Data.Sequence as Seq
+import qualified Df1
+import qualified Di.Core as Di
