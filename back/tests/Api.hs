f4b3dec3ee326f54fb7c7a3dfe914ff51817eb6b
Dmitrij Pavluk <al.lastor@bk.ru>
2019-01-30 11:06:27 +0300

    add api tests

--- back/tests/Api.hs
+++ back/tests/Api.hs
@@ -22,11 +22,10 @@ apiTests = H.describe "api" $ do
   H.it "fail request" $ do
     request <- makeRequest
       (Host "http://localhost/fail")
-      (Port 4400)
       (Method "GET")
     Status 404 "Not Found" <- runFailRequest request
     pure ()
-  H.describe "category" $ do
+  H.describe "Categories" $ do
     H.it "get categories request" $ do
       [] <- getCategoriesRequest
       pure ()
@@ -39,40 +38,67 @@ apiTests = H.describe "api" $ do
       let Uid categoryId = cciId categoryInfo
       request <- makeRequest
         (Host $ "http://localhost/category/" <> T.unpack categoryId)
-        (Port 4400)
         (Method "GET")
-      _ :: (Status, CCategoryFull) <- runRequest request
+      (Status 200 "OK", _ :: CCategoryFull) <- runRequest request
       pure ()
+
     H.it "delete category by id" $ do
       categoryInfo <- head <$> getCategoriesRequest
       Just True    <- deleteCategory (cciId categoryInfo)
+      Just False   <- deleteCategory (cciId categoryInfo)
       []           <- getCategoriesRequest
       pure ()
+
     H.it "modify notes of category" $ do
-      categoryId <- postCreateCategory
-      let Uid tCategoryId = categoryId 
-      request <- makeRequest
-        (Host $ "http://localhost/category/" <> T.unpack tCategoryId <> "/notes")
-        (Port 4400)
-        (Method "PUT")
-      let req = setRequestBodyJSON (makeEditObject "" "string") request
-      Status 200 _ <- runRequestNoBody req
-      Status 409 "Merge conflict occurred" <- runRequestNoBody req
-      void $ deleteCategory categoryId
-      Status 404 _ <- runRequestNoBody req
+      req <- withCategory $ \categoryId -> do
+        let Uid tCategoryId = categoryId 
+        request <- makeRequest
+          (Host $ "http://localhost/category/" <> T.unpack tCategoryId <> "/notes")
+          (Method "PUT")
+        let req = setRequestBodyJSON (makeEditObject "" "string") request
+        Status 200 "OK"                      <- runRequestNoBody req
+        Status 409 "Merge conflict occurred" <- runRequestNoBody req
+        pure req
+      Status 404 "Category not found"      <- runRequestNoBody req
       pure ()
+
     H.it "modify info of category" $ do
-      categoryId <- postCreateCategory
-      let Uid tCategoryId = categoryId 
-      request <- makeRequest
-        (Host $ "http://localhost/category/" <> T.unpack tCategoryId <> "/info")
-        (Port 4400)
-        (Method "PUT")
-      let req = setRequestBodyJSON makeEditCategoryInfo request
-      Status 200 _ <- runRequestNoBody req
-      void $ deleteCategory categoryId
-      Status 404 _ <- runRequestNoBody req
+      req <- withCategory $ \categoryId -> do
+        let Uid tCategoryId = categoryId 
+        request <- makeRequest
+          (Host $ "http://localhost/category/" <> T.unpack tCategoryId <> "/info")
+          (Method "PUT")
+        let req = setRequestBodyJSON makeEditCategoryInfo request
+        Status 200 "OK"                 <- runRequestNoBody req
+        pure req
+      Status 404 "Category not found" <- runRequestNoBody req
       pure ()
+
+  H.describe "Items" $ do
+    H.it "create & delete item" $
+      withCategory $ \categoryId -> do
+        itemId      <- postCreateItem categoryId
+        Just True   <- deleteItem itemId
+        Just False  <- deleteItem itemId
+        pure ()
+  
+    H.it "get item by id" $ do
+      withCategory $ \categoryId -> do
+        Uid itemId  <- postCreateItem categoryId
+        request     <- makeRequest
+          (Host $ "http://localhost/item/" <> T.unpack itemId)
+          (Method "GET")
+        (Status 200 "OK", _ :: CItemFull) <- runRequest request
+        void $ deleteItem (Uid itemId)
+        Status 404 "Item not found" <- runFailRequest request
+        pure ()
+
+withCategory :: (Uid Category -> IO a) -> IO a
+withCategory f = do
+  categoryId  <- postCreateCategory
+  res <- f categoryId
+  void $ deleteCategory categoryId
+  pure res
 
 makeEditCategoryInfo :: Value
 makeEditCategoryInfo = object
@@ -92,7 +118,6 @@ getCategoriesRequest :: IO [CCategoryInfo]
 getCategoriesRequest = do
   request <- makeRequest
     (Host "http://localhost/categories")
-    (Port 4400)
     (Method "GET")
   snd <$> runRequest request
 
@@ -100,7 +125,6 @@ postCreateCategory :: IO (Uid Category)
 postCreateCategory = do
   request <- makeRequest
     (Host "http://localhost/category?title=NewCategory&group=Model")
-    (Port 4400)
     (Method "POST")
   snd <$> runRequest request
 
@@ -108,13 +132,30 @@ deleteCategory :: Uid Category -> IO (Maybe Bool)
 deleteCategory (Uid categoryId) = do
   request <- makeRequest
     (Host $ "http://localhost/category/" <> T.unpack categoryId)
-    (Port 4400)
     (Method "DELETE")
   res <- runRequestNoBody request
   pure $ case res of
-    Status 200 _                    -> Just True
+    Status 200 "OK"                 -> Just True
     Status 404 "Category not found" -> Just False
     _                               -> Nothing
+
+deleteItem :: Uid Item -> IO (Maybe Bool)
+deleteItem (Uid itemId) = do
+  request <- makeRequest
+    (Host $ "http://localhost/item/" <> T.unpack itemId)
+    (Method "DELETE")
+  res <- runRequestNoBody request
+  pure $ case res of
+    Status 200 "OK"             -> Just True
+    Status 404 "Item not found" -> Just False
+    _                           -> Nothing
+
+postCreateItem :: Uid Category -> IO (Uid Item)
+postCreateItem (Uid categoryId) = do
+  request <- makeRequest
+    (Host $ "http://localhost/item/" <> T.unpack categoryId <> "?name=testName")
+    (Method "POST")
+  snd <$> runRequest request
 
 runRequestNoBody, runFailRequest :: Request -> IO Status
 runRequestNoBody request = getResponseStatus <$> httpNoBody request
@@ -129,9 +170,9 @@ newtype Host   = Host String
 newtype Port   = Port Int
 newtype Method = Method S8.ByteString
 
-makeRequest :: MonadThrow m => Host -> Port -> Method -> m Request
-makeRequest (Host host) (Port port) (Method method) = do
+makeRequest :: MonadThrow m => Host -> Method -> m Request
+makeRequest (Host host) (Method method) = do
   initReq <- parseRequest host
   pure $
-    setRequestPort port $
+    setRequestPort 4400 $
     setRequestMethod method initReq
