960baedab527fc68f442c86acee91beaea31196d
Artyom Kazak <yom@artyom.me>
2019-02-17 17:24:08 +0100

    Remove loggers we're not going to use

--- back/src/Guide/Logger/Methods.hs
+++ back/src/Guide/Logger/Methods.hs
@@ -4,10 +4,10 @@
 module Guide.Logger.Methods
 (
   -- * Loggers in 'MonadDi'
-  debugT, infoT, noticeT, warningT, errorT, alertT, criticalT, emergencyT,
+  debugT, infoT, warningT, errorT,
 
   -- * Loggers in 'IO'
-  debugIO, infoIO, noticeIO, warningIO, errorIO, alertIO, criticalIO, emergencyIO,
+  debugIO, infoIO, warningIO, errorIO,
 
   -- * Utils
   push, attr, value,
@@ -21,27 +21,17 @@ import qualified Di.Core as DC
 import qualified Di
 import Guide.Logger.Types
 
-debugT, infoT, noticeT, warningT, errorT, alertT, criticalT, emergencyT
-  :: Di.MonadDf1 m => Text -> m ()
+debugT, infoT, warningT, errorT :: Di.MonadDf1 m => Text -> m ()
 debugT     msg = Di.debug     . Df1.message $ toLText msg
 infoT      msg = Di.info      . Df1.message $ toLText msg
-noticeT    msg = Di.notice    . Df1.message $ toLText msg
 warningT   msg = Di.warning   . Df1.message $ toLText msg
 errorT     msg = Di.error     . Df1.message $ toLText msg
-alertT     msg = Di.alert     . Df1.message $ toLText msg
-criticalT  msg = Di.critical  . Df1.message $ toLText msg
-emergencyT msg = Di.emergency . Df1.message $ toLText msg
 
-debugIO, infoIO, noticeIO, warningIO, errorIO, alertIO, criticalIO, emergencyIO
-  :: DefDi -> Text -> IO ()
+debugIO, infoIO, warningIO, errorIO :: DefDi -> Text -> IO ()
 debugIO     di msg = DC.log di Df1.Debug     . Df1.message $ toLText msg
 infoIO      di msg = DC.log di Df1.Info      . Df1.message $ toLText msg
-noticeIO    di msg = DC.log di Df1.Notice    . Df1.message $ toLText msg
 warningIO   di msg = DC.log di Df1.Warning   . Df1.message $ toLText msg
 errorIO     di msg = DC.log di Df1.Error     . Df1.message $ toLText msg
-alertIO     di msg = DC.log di Df1.Alert     . Df1.message $ toLText msg
-criticalIO  di msg = DC.log di Df1.Critical  . Df1.message $ toLText msg
-emergencyIO di msg = DC.log di Df1.Emergency . Df1.message $ toLText msg
 
 -- | Push method name to the log path.
 push :: Di.MonadDf1 m => Text -> m a -> m a
