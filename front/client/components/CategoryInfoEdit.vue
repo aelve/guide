b8e8630ce8c8f3eb65c3a291ac84695fe1b5120e
Giyos <giyos.a@gmail.com>
2019-03-27 13:18:57 +0500

    separately import vue, vue-class-component

--- front/client/components/CategoryInfoEdit.vue
+++ front/client/components/CategoryInfoEdit.vue
@@ -79,7 +79,9 @@
 </template>
 
 <script lang="ts">
-import { Vue, Component, Prop, Watch } from 'vue-property-decorator';
+import Vue from 'vue'
+import Component from 'vue-class-component'
+import { Prop, Watch } from 'vue-property-decorator';
 import { CategoryStatus } from 'client/service/Category'
 
 @Component
