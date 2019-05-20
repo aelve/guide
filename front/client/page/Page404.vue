d2a6db2423ed433bfa7b4102a7f68aced33a36d0
avele <34437766+avele@users.noreply.github.com>
2019-05-20 23:19:38 +0400

    404 page (#293)

---
+++ front/client/page/Page404.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="page404 pt-5">
+    <h1 class="display-4 page404__number-header font-weight-bold">404</h1>
+    <h2 class="display-2 mt-4">
+      Page Not Found
+    </h2>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+
+@Component
+export default class Page404 extends Vue { }
+</script>
+
+<style lang="postcss" scoped>
+.page404 {
+  text-align: center;
+  display: flex;
+  height: 100%;
+  flex: 1;
+  flex-direction: column;
+  align-items: center;
+
+  &__number-header {
+    letter-spacing: 1.2rem !important;
+  }
+}
+</style>
+
--- front/postcss.config.js
+++ front/postcss.config.js
@@ -1,13 +1,8 @@
-// TODO configure
-
-const autoprefixer = require('autoprefixer')
-
-const plugins = [
-  autoprefixer({
-    browsers: ['last 3 versions', '> 2%', 'ie >= 9', 'Firefox >= 30', 'Chrome >= 30']
-  })
-]
 
 module.exports = {
-  plugins
+  plugins: [
+    require('autoprefixer'),
+    require('postcss-preset-env'),
+    require('precss')
+  ]
 }
