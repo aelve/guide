0a52edcc77f96f6db143901d2e8e01dfa9b09b88
Avele <kotoraxia@gmail.com>
2019-07-13 17:42:31 +0400

    Unused imports removed

--- front/client/components/CategoryItem.vue
+++ front/client/components/CategoryItem.vue
@@ -103,7 +103,6 @@
 import Vue from 'vue'
 import Component from 'vue-class-component'
 import { Prop } from 'vue-property-decorator'
-import { ICategoryItem } from 'client/service/CategoryItem.ts'
 import CategoryItemToolbar from 'client/components/CategoryItemToolbar.vue'
 import CategoryItemSection from 'client/components/CategoryItemSection.vue'
 import CategoryItemTraits from 'client/components/CategoryItemTraits.vue'
