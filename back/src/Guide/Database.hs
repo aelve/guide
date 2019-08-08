3512b77ba13d3289970a2fa4bbe0753daf552967
Artyom Kazak <artyom@aelve.com>
2019-08-08 18:25:55 +0300

    Reexport more modules from Guide.Database

--- back/src/Guide/Database.hs
+++ back/src/Guide/Database.hs
@@ -1,9 +1,13 @@
 module Guide.Database
 (
   module Guide.Database.Schema,
-  module Guide.Database.Get
+  module Guide.Database.Get,
+  module Guide.Database.Add,
+  module Guide.Database.Set,
 )
 where
 
 import Guide.Database.Schema
 import Guide.Database.Get
+import Guide.Database.Add
+import Guide.Database.Set
