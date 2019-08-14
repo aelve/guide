86a7d781ef0c9c41eaa0e0dbea9dac1475e1d99f
willbasky <vladislav.sabanov@gmail.com>
2019-08-14 20:55:50 +0500

    fix forgotten

--- back/src/Guide/Database/Add.hs
+++ back/src/Guide/Database/Add.hs
@@ -65,6 +65,7 @@ addCategory
             , notes
             , enabled_sections
             , items_order
+            , deleted
             )
           VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9)
         |]
