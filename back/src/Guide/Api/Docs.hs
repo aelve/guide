7869d1d312969ccfba9ef4131c8f1c574a35d883
Artyom Kazak <artyom@aelve.com>
2019-08-19 17:58:24 +0300

    Generate swagger.json for branches (#380)
    * Generate swagger.json for branches
    
    * Fix
    
    * Fix again
    
    * Fix once more
    
    * [skip ci] Regenerate swagger.json
    
    * Don't log debug info to stdout
    
    * Push even if the branch is ahead
    
    * Fix
    
    * Empty
    
    * [skip ci] Regenerate swagger.json
    
    * Don't use "skip ci"

---
+++ back/src/Guide/Api/Docs.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+-- | Rendered documentation for the API.
+module Guide.Api.Docs
+(
+  apiSwaggerDoc,
+  apiSwaggerRendered,
+)
+where
+
+import Imports
+
+import Servant.Swagger (toSwagger)
+import Data.Swagger
+import qualified Data.Aeson.Encode.Pretty as AesonPretty
+
+import Guide.Api.Types
+
+----------------------------------------------------------------------------
+-- Swagger
+----------------------------------------------------------------------------
+
+-- | Swagger docs for the 'Api'.
+apiSwaggerDoc :: Swagger
+apiSwaggerDoc =
+  toSwagger (Proxy @Api)
+    & info.title   .~ "Aelve Guide API"
+    & info.version .~ "alpha"
+
+-- | Pretty-printed @swagger.json@ for the 'Api'.
+apiSwaggerRendered :: Text
+apiSwaggerRendered = utf8ToText $ AesonPretty.encodePretty apiSwaggerDoc
