4bdb76f2e730a887bec81ddaae16287826b127bb
Artyom Kazak <artyom@aelve.com>
2019-09-09 23:57:35 +0300

    Commit the JS bundle for the old frontend into the repo

--- back/guidejs/src/index.js
+++
@@ -1,9 +0,0 @@
-import jquery from 'jquery';
-// export jQuery to the globals.
-window.$ = window.jquery = window.jQuery = jquery;
-
-import csrfProtection from './csrfProtection.js';
-
-export default { 
-  csrfProtection
-};
