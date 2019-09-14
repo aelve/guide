2bf6ef96174ded2ab58c709a9e5698a31554667f
Avele <kotoraxia@gmail.com>
2019-09-14 19:39:03 +0400

    Search results page restyle

--- front/client/page/SearchResults.vue
+++ front/client/page/SearchResults.vue
@@ -1,50 +1,77 @@
 <template>
   <v-container class="search-result-container">
     <template v-if="results && results.length">
-      <v-card
-        v-for="(result, index) in results"
-        :key="index"
-        class="search-result-card"
-      >
 
-        <v-card-title v-if="result.tag === 'Category'">
-          <span class="search-result-title">
-            <span class="search-result-group-name">
-              {{ result.contents.info.group }}
-            </span>
-              »
+      <template v-if="resultsCategories && resultsCategories.length">
+        <h1 class="mt-0 mb-6"> Categories </h1>
+
+        <v-card
+          v-for="result in resultsCategories"
+          :key="result.contents.info.id"
+          class="search-result-card"
+        >
+
+          <v-card-title class="flex-column align-baseline">
             <router-link
               openInNewTab
+              class="search-results__result-title"
               :to="`/haskell/${result.contents.info.id}`"
-            >{{ result.contents.info.title }}</router-link>
-          </span>
-        </v-card-title>
+            >
+              <h2 class="my-0">{{ result.contents.info.title }}</h2>
+            </router-link>
 
-        <v-card-title v-else>
-          <span class="search-result-title">
+            <div class="search-results__result-parent-wrap">in <span class="search-results__result-parent-title">{{result.contents.info.group}}</span></div>
+          </v-card-title>
+          <v-card-text
+            v-if="result.contents.description"
+            v-html="result.contents.description.html"
+          />
+
+        </v-card>
+
+      </template>
+
+      <template v-if="resultsItems && resultsItems.length">
+        <h1 class="mt-10 mb-6"> Items </h1>
+
+        <v-card
+          v-for="result in resultsItems"
+          :key="result.contents.info.id"
+          class="search-result-card"
+        >
+
+          <v-card-title class="flex-column align-baseline">
             <router-link
               openInNewTab
-              :to="`/haskell/${result.contents.category.id}`"
-            >{{ result.contents.category.title }}</router-link>
-              »
-            <span>
+              class="search-results__result-title"
+              :to="`/haskell/${result.contents.category.id}#item-${result.contents.info.id}`"
+            >
+              <h2 class="my-0">{{ result.contents.info.name }}</h2>
+            </router-link>
+
+            <div class="search-results__result-parent-wrap">in <router-link :to="`/haskell/${result.contents.category.id}`" class="search-results__result-parent-title">{{result.contents.category.title}}</router-link></div>
+            <!-- <span class="search-result-title">
               <router-link
                 openInNewTab
-                :to="`/haskell/${result.contents.category.id}#item-${result.contents.info.id}`"
-              >{{ result.contents.info.name }}</router-link>'s ecosystem
-            </span>
-          </span>
-        </v-card-title>
+                :to="`/haskell/${result.contents.category.id}`"
+              >{{ result.contents.category.title }}</router-link>
+                »
+              <span>
+                <router-link
+                  openInNewTab
+                  :to="`/haskell/${result.contents.category.id}#item-${result.contents.info.id}`"
+                >{{ result.contents.info.name }}</router-link>'s ecosystem
+              </span>
+            </span> -->
+          </v-card-title>
+          <v-card-text
+            v-if="result.contents.ecosystem"
+            v-html="result.contents.ecosystem.html"
+          />
 
-        <v-card-text
-          v-if="result.tag === 'Category' && result.contents.description"
-          v-html="result.contents.description.html"
-        />
-        <v-card-text
-          v-if="result.tag === 'Item' && result.contents.ecosystem"
-          v-html="result.contents.ecosystem.html"
-        />
-      </v-card>
+        </v-card>
+      </template>
+
     </template>
 
     <div
@@ -61,12 +88,25 @@ import Vue from 'vue'
 import Component from 'vue-class-component'
 import { Prop, Watch } from 'vue-property-decorator'
 
+const resultTagCodes = {
+  category: 'Category',
+  item: 'Item'
+}
+
 @Component
 export default class SearchResults extends Vue {
   @Prop(String) query!: string
 
   get results () {
     return this.$store.state.wiki.searchResults
+  }
+
+  get resultsCategories () {
+    return this.results.filter(x => x.tag === resultTagCodes.category)
+  }
+
+  get resultsItems () {
+    return this.results.filter(x => x.tag === resultTagCodes.item)
   }
 
   beforeMount () {
@@ -93,19 +133,34 @@ export default class SearchResults extends Vue {
 }
 </script>
 
-<style scoped>
+<style lang="postcss" scoped>
 @media only screen and (min-width: 1264px) {
   .search-result-container {
     max-width: 900px;
   }
 }
-.search-result-card:not(:last-child) {
-  margin-bottom: 1.5em;
+.search-result-card {
+  .v-card__text {
+    color: #000;
+  }
+
+  &:not(:last-child) {
+    margin-bottom: 1.5em;
+  }
 }
-.search-result-title {
-  font-size: 22px;
+.search-results__result-title {
+  word-break: break-word;
+}
+.search-results__result-parent-wrap {
+  font-size: 1rem;
+}
+.search-results__result-parent-title {
   font-weight: 600;
-  line-height: 28px;
+  padding-left: 0;
+  line-height: 1;
+  max-width: 100%;
+  overflow: visible;
+  word-break: break-all
 }
 .search-result-group-name {
   color: #797979;
